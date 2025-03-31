610e72cafae0db712500c4d96e0c4301ad947472
Anshum <anshumjani.2004@gmail.com>
2025-03-31 17:00:48 +0530

    Fifth Commit

--- Binary Tree/10leafatsame.py
+++ Binary Tree/10leafatsame.py
@@ -1,9 +1,44 @@
-from collections import 
-class Node:
+from collections import deque
+class Node():
     def __init__(self,data):
         self.data=data
         self.left=None
         self.right=None
 
 
-def leaf(root):
+def check_level_of_leave(root):
+    if not root:
+        return True
+    queue=deque([(root,0)])
+    leaf_level = -1
+
+    while queue:
+        node, level = queue.popleft()
+
+        if not node.left and not node.right:
+            if leaf_level == -1:
+                leaf_level = level
+            elif leaf_level != level:
+                return False
+            
+        if node.left:
+            queue.append((node.left,level + 1))
+        if node.right:
+            queue.append((node.right,level +1 ))
+
+
+    return True
+
+
+root = Node(10)
+root.left = Node(20)
+root.right = Node(30)
+root.left.left = Node(10)
+root.left.right = Node(15)
+
+
+print(check_level_of_leave(root))
+                             
+            
+
+
